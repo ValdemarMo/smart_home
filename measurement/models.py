d973c92c911cef3e7570ee919b81f28eb0b2cfb0
Mikhail Vinogradov <movaldemar@gmail.com>
2023-05-11 16:54:07 +0300

    go

--- measurement/models.py
+++ measurement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100, default=None)
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey('Sensor', related_name='measurements', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    temperature = models.DecimalField(max_digits=5, decimal_places=2)
+    # image = models.ImageField(null=True)
+
--- measurement/views.py
+++ measurement/views.py
@@ -1,2 +1,14 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+
+
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+# Создать датчик. Указываются название и описание датчика.
+# Изменить датчик. Указываются название и описание.
+# Добавить измерение. Указываются ID датчика и температура.
+# Получить список датчиков. Выдаётся список с краткой информацией по датчикам: ID, название и опис
+
+
+@api_view
+def
